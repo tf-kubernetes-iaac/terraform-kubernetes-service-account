fe965aab1b47a73045f8ceef9451baa28fa51676
github_username_here <email@example.com>
2026-01-14 17:00:34 +0545

    Name and Namespace variables for Service Account are added.

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  type = string
+  description = "Enter the name of service account that you want to create."
+}
+
+variable "namespace" {
+    type = string
+    description = "Enter the namespace for which you want to create Service account for."
+  
+}
